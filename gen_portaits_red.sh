c85b5dfca5834852425771879fa334108b2dd4c5
johann1764 <johann.brault_baron@yahoo.com>
2025-05-21 10:27:04 +0200

    only update red when necessary

--- gen_portaits_red.sh
+++ gen_portaits_red.sh
@@ -9,13 +9,15 @@ find "$SRC" -type d | while IFS= read -r line; do
 done
 
 find "$SRC" -name '*.png' -o -name '*.webp' | while IFS= read -r line; do
-   res=$(identify -verbose "$line" | grep -m 1 'geometry: 400x')
-   if [ "$res" != "" ] ; then
-      geom=$( echo "$res" | sed "s/^.*geometry: \([0-9]*x[0-9]*\).*$/\1/")
-      W=$(echo "$geom" | cut -dx -f1)
-      H=$(echo "$geom" | cut -dx -f2)
-      newgeom=$(expr $W / 2)x$(expr $H / 2)
-      echo "$line:$geom -> ${DST}${line/${SRC}}:$newgeom"
-      convert -scale "$newgeom" "$line" ${DST}${line/${SRC}}
+   if [ "$line" -nt "$0" ] ; then
+      res=$(identify -verbose "$line" | grep -o -m 1 'geometry: 400x[0-9][0-9]*')
+      if [ "$res" != "" ] ; then
+         dst=${DST}${line/${SRC}}
+         geom=$( echo "$res" | sed "s/^.*geometry: 400x\([0-9][0-9]*\)$/\1/")
+         newgeom=200x$(expr $geom / 2)
+         echo "$line:400x$geom -> $dst:$newgeom"
+         convert -scale "$newgeom" "$line" "$dst"
+      fi
    fi
 done
+touch $0
